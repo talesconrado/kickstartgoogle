6ef365166da5bed6a5dc22f4b1abdadcfca44a87
Tales C <46583366+talesconrado@users.noreply.github.com>
2019-03-07 15:09:03 -0300

    Add files via upload

---
+++ 2018 - Round A - Even Digits.py
@@ -0,0 +1,64 @@
+testes = int(input())
+dataset = []
+for i in range(testes):
+    dataset.append(input())
+
+
+def bonito(numero):
+    numero = str(numero)
+    for i in range(len(numero)):
+        if int(numero[i]) % 2 == 0:
+            pass
+        else:
+            return False
+    return True
+
+def onde_esta_feio(numero):
+    numero = str(numero)
+    for i in range(len(numero)):
+        if bonito(numero[i]):
+            pass
+        else:
+            return i
+            break
+    return -1
+
+
+def embelezar_up(numero):
+    if onde_esta_feio(numero) == -1:
+        return 0
+    else:
+        lista = [int(x) for x in str(numero)]
+        partida = onde_esta_feio(numero)
+        
+        lista[partida] += 1
+        
+        for i in range(partida+1,len(lista)):
+            lista[i] = 0
+        resposta = ''.join(str(x) for x in lista)
+        distancia = abs(int(numero) - int(resposta))
+        return distancia
+
+def embelezar_down(numero):
+    if onde_esta_feio(numero) == -1:
+        return 0
+    else:
+        lista = [int(x) for x in str(numero)]
+        partida = onde_esta_feio(numero)
+        lista[partida] -= 1
+        for i in range(partida+1, len(lista)):
+            lista[i] = 8   
+        resposta = ''.join(str(x) for x in lista)
+        distancia = abs(int(numero) - int(resposta))
+        return distancia
+
+def mais_proximo(numero):
+    if embelezar_up(numero) > embelezar_down(numero):
+        return embelezar_down(numero)
+    else:
+        return embelezar_up(numero)      
+
+
+for i in range(len(dataset)):
+    num = mais_proximo(dataset[i])
+    print('Case #' + str(i+1) + ': ' + str(num))
