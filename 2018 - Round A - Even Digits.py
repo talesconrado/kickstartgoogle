960633fad0576f91588785374729de37f41710ef
Tales C <46583366+talesconrado@users.noreply.github.com>
2019-08-22 18:04:50 -0300

    Update 2018 - Round A - Even Digits.py

--- 2018 - Round A - Even Digits.py
+++ 2018 - Round A - Even Digits.py
@@ -1,65 +1,72 @@
-"""NÃO ESTÁ FUNCIONANDO"""
-
-def bonito(numero):
-    numero = str(numero)
-    for i in range(len(numero)):
-        if int(numero[i]) % 2 == 0:
-            pass
-        else:
-            return False
-    return True
-
-def onde_esta_feio(numero):
-    numero = str(numero)
-    for i in range(len(numero)):
-        if bonito(numero[i]):
-            pass
-        else:
-            return i
-            break
-    return -1
-
-
-def embelezar_up(numero):
-    if onde_esta_feio(numero) == -1:
-        return 0
-    else:
-        lista = [int(x) for x in str(numero)]
-        partida = onde_esta_feio(numero)
-        
-        lista[partida] += 1
-        
-        for i in range(partida+1,len(lista)):
-            lista[i] = 0
-        resposta = ''.join(str(x) for x in lista)
-        distancia = abs(int(numero) - int(resposta))
-        return distancia
-
-def embelezar_down(numero):
-    if onde_esta_feio(numero) == -1:
-        return 0
-    else:
-        lista = [int(x) for x in str(numero)]
-        partida = onde_esta_feio(numero)
-        lista[partida] -= 1
-        for i in range(partida+1, len(lista)):
-            lista[i] = 8   
-        resposta = ''.join(str(x) for x in lista)
-        distancia = abs(int(numero) - int(resposta))
-        return distancia
-
-def mais_proximo(numero):
-    if embelezar_up(numero) > embelezar_down(numero):
-        return embelezar_down(numero)
-    else:
-        return embelezar_up(numero)      
-
+def embelezar_down(num):
+    n = list(num)
+    for i in range(len(n)):
+        if str(n[i]) in '13579':
+            n[i] = int(n[i]) - 1
+            for j in range(i+1,len(n)):
+                n[j] = 8
     
+    m = ''.join(str(a) for a in n)
+    return int(num) - int(m)
+    
+def embelezar_up(num):
+    n = list(num)
+    for i in range(len(n)):
+        if str(n[i]) in '1357':
+            if n[i-1] == '8':
+                z = i - 1
+                while z > 0 and n[z] == '8':
+                    n[z] = 0
+                if z > 0:
+                    n[z-1] = int(n[z-1]) + 2
+            n[i] = int(n[i]) + 1
+            for j in range(i+1,len(n)):
+                n[j] = 0
+        elif str(n[i]) == '9':
+            if i > 0:
+                if n[i-1] == '8':
+                    z = i - 1
+                    while z >= 0 and n[z] == '8':
+                        n[z] = 0
+                        z -= 1
+                    if z >= 0:
+                        n[z] = int(n[z]) + 2
+                else:
+                    n[i-1] = int(n[i-1]) + 2
+                
+                n[i] = 0
+                for j in range(i+1,len(n)):
+                    n[j] = 0
+                    
+            elif len(n) == 1:
+                n[i] = 10
+            
+            else:
+                n[i] = 0
+                for j in range(i+1,len(n)):
+                    n[j] = 0
+                    
+    if len(n) > 1:
+        if n[0] == 0 and n[1] == 0:
+            n = [2] + n[:]
+    
+    m = ''.join(str(a) for a in n)
+    return int(m) - int(num)
+
+def embelezar(n):
+    m = embelezar_down(n)
+    p = embelezar_up(n)
+    if m > p:
+        return p
+    else:
+        return m
+
 testes = int(input())
 dataset = []
 for i in range(testes):
     dataset.append(input())
 
 for i in range(len(dataset)):
-    num = mais_proximo(dataset[i])
+    num = embelezar(dataset[i])
     print('Case #' + str(i+1) + ': ' + str(num))
+print()
